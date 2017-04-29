ddbf23c172f146df1f91df5f87c647e894753e37
Vadim Zeitlin <vadim@zeitlins.org>
2017-04-29 18:51:20 +0200

    Resolve codes to full voter names
    This not only validates the codes (which should be already valid anyhow),
    but also ensures that voting with and without code counts only once.

--- elections57_results.py
+++ elections57_results.py
@@ -53,6 +53,58 @@ def get_credentials():
         print 'Storing credentials to ' + credential_path
     return credentials
 
+import cPickle
+import requests
+
+class code_cache:
+    # Just hard-code it for now.
+    cache_file_name = '57codes.cache'
+
+    code_to_voter = {}
+
+    """Allows to lookup a voter by their code and caches them in a file to
+    speed up the next execution of the script.
+    """
+
+    def __init__(self):
+        """Try to load the cached from file."""
+        try:
+            with open(self.cache_file_name, 'rb') as f:
+                self.code_to_voter = cPickle.load(f)
+        except IOError:
+            # Ignore the absence of the cache.
+            pass
+
+    def __del__(self):
+        """Save the cache when this object is destroyed."""
+        if self.code_to_voter:
+            try:
+                with open(self.cache_file_name, 'wb') as f:
+                    cPickle.dump(self.code_to_voter, f)
+            except IOError as e:
+                print >> sys.stderr, 'Failed to save code cache:', e
+
+    def get_voter_id(self, code):
+        """Returns the voter id of the form "<class>-<name>" for the given
+        code.
+
+        Throws if the code is invalid.
+        """
+        if code in self.code_to_voter:
+            voter_id = self.code_to_voter[code]
+        else:
+            voter_id = self._get_id_from_code(code)
+            self.code_to_voter[code] = voter_id
+        return voter_id
+
+    def _get_id_from_code(self, code):
+        res = requests.post('http://auth.alumni57.ru/api/v1/check_code', data={'code': code})
+        res.raise_for_status()
+        d = res.json()
+        if d['status'] != 'ok':
+            raise Exception('Bad code {}: status: {}'.format(code, d['status']))
+        return u'{}{}-{}'.format(d['year'], d['letter'], d['full_name'])
+
 def get_raw_candidates():
     """Returns a dict containing the voter ID as keys and the list of
     the candidates voted for as values.
@@ -80,25 +132,28 @@ def get_raw_candidates():
     col_candidates, col_bylaws, col_comment = range(13)
 
     candidates = {}
+    cc = code_cache()
     row_num = 0
     for row in data:
         row_num += 1
 
-        # If we have the unique code, use its class and name parts as voter ID.
+        # If we have the unique code, obtain the voter ID from it.
         code = row[col_code]
         if code:
-            # The code format is "57-1234v-wyxz-0123456789abcdef" except that
-            # there can be 5 or even 6 letters depending on transliteration
-            # vagaries, so we can't rely on its index.
-            start = 3
-            end = code.find('-', start + 9)
-            if end == -1:
+            # Code should normally be in "temp_code (masked_perm_code)" form,
+            # but accept not masked permanent codes too, just in case we have
+            # any bugs preventing the code replacing them with temporary codes
+            # from running again.
+            end_temp_code = code.find(' (')
+            if end_temp_code != -1:
+                code = code[0:end_temp_code]
+            voter_id = cc.get_voter_id(code)
+        else:
+            if not row[col_class]:
                 print >> sys.stderr,\
-                    'Skipping row {}: invalid code format "{}"'.format(row_num, code)
+                    'Skipping row {}: no class specified'.format(row_num)
                 next
 
-            voter_id = code[start:end]
-        else:
             # Make up an ID from the class and the name.
             voter_id = row[col_class] + '-'
             if row[col_name]:
